a7a3d52c347eae85c6b71d45a9e5b100f93822fa
ipsitasarangi21-ux <ipsitasarangi21@gmail.com>
2026-07-02 12:00:58 +0000

    Initial Commit Model Build

---
+++ airbnb/models/src/src_hosts.sql
@@ -0,0 +1,14 @@
+WITH raw_HOSTS AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.raw_HOSTS
+)
+SELECT
+    ID AS HOST_ID,
+    NAME AS HOST_NAME,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM
+    RAW_HOSTS
